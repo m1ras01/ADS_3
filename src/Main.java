d72cbf94376d4994d54fc86ea5d4d979c70110b0
m1ras01 <221930@astanait.edu.kz>
2023-05-01 20:37:35 +0600

    i ad MyArrayList and LinkedList

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+         .out.println("Hello world!");
+
+        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> inter = new ArrayDeque<>();
+        Sta
+        ck<Integer> stack
     }
 }
