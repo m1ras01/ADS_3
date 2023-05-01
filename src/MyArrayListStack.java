d473f7b6dd967d2a40e62676e79764111e32c24f
m1ras01 <221930@astanait.edu.kz>
2023-05-01 21:40:46 +0600

    .

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -8,5 +8,24 @@ public class MyArrayListStack<E> {
     public void push(E element) {
         array.add(element);
     }
+    public E pop() {
+        checkEmpty();
+        return (E) array.remove(array.size() - 1);
+    }
+    public void checkEmpty(){
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+    }
+    public E peek() {
+        checkEmpty();
+        return (E) array.get(array.size() - 1);
+    }
+    public boolean isEmpty() {
+        return array.size() == 0;
+    }
+    public int size() {
+        return array.size();
+    }
 
 }
