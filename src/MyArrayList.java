25b1cb6d009282467e8772ca7c22059a55263e06
m1ras01 <221930@astanait.edu.kz>
2023-05-01 20:40:44 +0600

    method add and increaseBuffer

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,2 +1,22 @@
 public class MyArrayList {
+    private Object[] arr;
+    private int size = 0;
+
+    MyArrayList(){
+        this.arr = new Object[5];
+        this.size = 0;
+    }
+    public void add(Object item) {
+        if(size == arr.length){
+            increaseBuffer();
+        }
+        arr[size++] = item;
+    }
+    public void increaseBuffer(){
+        Object[] newArr = new Object[arr.length*2];
+        for(int i=0; i< arr.length; i++){
+            newArr[i]=arr[i];
+        }
+        arr = newArr;
+    }
 }
